a3d9555333fd10d8994bb4073df5e8c8d5562f5a
arveymenon <arveymenon@gmail.com>
2021-11-12 11:50:20 +0530

    Update

--- scraper.js
+++ scraper.js
@@ -15,6 +15,7 @@ const fetchData = async (url) => {
     return cheerio.load(result.data);
   }
   catch(e){
+    console.log(e)
     console.log("scraping failed for: "+url)
     return false
   }
@@ -24,15 +25,16 @@ const getResults = async () => {
   counter += 1
   // try{
     // Need to find the page
-    crawlOnAmazon()
-    crawlOnFlipkart()
+    // crawlOnAmazon()
+    // crawlOnFlipkart()
 
     // Couldn't set up
     // crawlOnCroma()
     // crawlOnRelianceDigital()
+    crawlOnShopAtSC()
 
-    crawlOnGamesTheShop()
-    crawlOnVijaySales()
+    // crawlOnGamesTheShop()
+    // crawlOnVijaySales()
     // }
   // catch(e){
   //   console.log(e)
@@ -59,23 +61,58 @@ const crawlOnAmazon = async () => {
       console.log(siteName)
       await console.log(available, available.size)
       sendResponse('Found On Amazon')
-
+      
       crawlOnAmazon()
     }else{
       setTimeout(()=>{
+        sendResponse('Found On Amazon')
         crawlOnAmazon()
-      },3000)
+      },10000)
     }
   }
   catch(err){
     console.log(err)
     setTimeout(()=>{
       crawlOnAmazon()
-    },3000)
+    },10000)
+  }
+}
+
+const crawlOnShopAtSC = async () => {
+  console.log('crawling on ShopATSC')
+  try {
+    const $ = await fetchData("https://shopatsc.com/collections/playstation-5/products/playstation-5-console-store");
+
+    // sendResponse('Found On Amazon')
+    if(typeof $ === 'function'){
+      let siteName = await $('#nav-logo-sprites').text();
+      
+      $('#product-add-to-cart').each((index, element) => {
+        if(element)
+          available.add($(element).text());
+      });
+      console.log(siteName)
+      await console.log(available, available.size)
+      sendResponse('Found On Amazon')
+      
+      crawlOnAmazon()
+    }else{
+      setTimeout(()=>{
+        sendResponse('Found On Amazon')
+        crawlOnAmazon()
+      },10000)
+    }
+  }
+  catch(err){
+    console.log(err)
+    setTimeout(()=>{
+      crawlOnAmazon()
+    },10000)
   }
 }
 
 const crawlOnCroma = async () => {
+  console.log('crawling on croma')
   try {
     const siteUrl2 = "https://www.croma.com/sony-playstation-5-825gb-ssd-cfi-1008a01r-white-/p/231643";
     const $ = await fetchData(siteUrl2);
@@ -89,6 +126,7 @@ const crawlOnCroma = async () => {
 
         available.add($(element).text());
       });
+      // available.add('Blah');
       
       // $('.out-of-stock-msg-pdp').each((index, element) => {
       //   console.log($(element).text())
@@ -116,6 +154,7 @@ const crawlOnCroma = async () => {
 }
 
 const crawlOnRelianceDigital = async () => {
+  console.log('crawling on reliance digital')
   try {
     const $ = await fetchData("https://www.reliancedigital.in/sony-playstation-5-console/p/491936180");
   
@@ -142,6 +181,7 @@ const crawlOnRelianceDigital = async () => {
 }
 
 const crawlOnGamesTheShop = async () => {
+  console.log('crawling on games the shop')
   try {
     const $ = await fetchData("https://www.gamestheshop.com/PlayStation-5-Console/5111");
   
@@ -171,6 +211,7 @@ const crawlOnGamesTheShop = async () => {
 }
 
 const crawlOnVijaySales = async () => {
+  console.log('crawling on vijay sales')
   try {
     const $ = await fetchData("https://www.vijaysales.com/Sony-PlayStation-5-1TB-Console-White-/15387");
   
@@ -205,6 +246,7 @@ const crawlOnVijaySales = async () => {
 }
 
 const crawlOnFlipkart = async () => {
+  console.log('crawling on flipkart')
   try {
     const $ = await fetchData("https://www.flipkart.com/sony-playstation-5-cfi-1008a01r-825-gb-astro-s-playroom/p/itma0201bdea62fa");
   
@@ -218,7 +260,7 @@ const crawlOnFlipkart = async () => {
       console.log(siteName)
       await console.log(available, available.size)
       
-      sendResponse('Found On Vijay Sales')
+      sendResponse('Found On Filpkart')
 
       crawlOnFlipkart()
     } else {
