644ddc2e04bed961e59de40c8e2bb5e9d889ec59
arveymenon <arveymenon@gmail.com>
2021-12-11 22:53:40 +0530

    key secured

--- notification.js
+++ notification.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var sendNotification = function(data) {
     var headers = {
       "Content-Type": "application/json; charset=utf-8"
@@ -12,7 +13,7 @@ var sendNotification = function(data) {
     };
     
     var https = require('https');
-    var req = https.request(options, function(res) {  
+    var req = https.request(options, function(res) {
       res.on('data', function(data) {
         // console.log("Response:");
         // console.log(JSON.parse(data));
@@ -30,9 +31,9 @@ var sendNotification = function(data) {
   
   const createNotification = (message) => {
     const notification = {
-      app_id: "11482de5-db43-43bd-aed4-9ebf163c569d",
+      app_id: process.env.ONESIGNAL_APP_ID,
       contents: {"en": message},
-      include_player_ids: ["6fab8e1c-3c2c-46fc-8596-1c52d96202b5"]
+      include_player_ids: [process.env.TEMP_USER_ID]
     };
     sendNotification(notification)
   }
